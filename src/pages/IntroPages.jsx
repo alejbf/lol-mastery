bbb1f7ec64610f071681b9a4a9f176971f0176b5
Alejandro <alejandrojbf19@gmail.com>
2026-02-08 20:39:20 +0100

    intro pages styles

--- src/pages/IntroPages.jsx
+++ src/pages/IntroPages.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { TextField } from "@mui/material"
+
 
 const IntroPages = () => {
+  const title ="lol mastery"
   return (
+    <>
     <div>
-      <h1>LoL Mastery</h1>
-      <h3>Search your champion mastery</h3>
+      <h1>{title.toUpperCase()}</h1>
+      <h3 className="text-white">Search your champion mastery</h3>
     </div>
+    <div className="mt-3">
+      <TextField id="outlined-basic" label="GAME NAME #TAG" variant="outlined" fullWidth />
+    </div>
+    </>
+    
   )
 }
 
